5d780c8314cc050e18d4b579b3bb70fe6754c18f
SaharHazan14 <saharhazan5@gmail.com>
2025-04-08 17:07:47 +0300

    commit-3

--- SaharHazan.java
+++ SaharHazan.java
@@ -1,7 +1,7 @@
 public class SaharHazan {
 	public static void main(String[] args) {
 		for(int i = 1; i <= 10; i++) {
-			System.out.println("HII Sahar Hazan ... " + i);
+			System.out.println("HI Sahar Hazan ... " + i);
 		}
 	}
 }
